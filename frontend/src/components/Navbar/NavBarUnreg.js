a112c0f3596a4897ec7636fe4d592ef04964f4f5
Tammy Truong <thtruong@usc.edu>
2022-11-14 21:53:16 -0800

    updated navbar styles. removed reportWebVitals.js and setupTests.js

--- frontend/src/components/Navbar/NavBarUnreg.js
+++ frontend/src/components/Navbar/NavBarUnreg.js
@@ -5,15 +5,42 @@ import { Link } from "react-router-dom";
 export default function NavBarUnreg() {
   return (
     <div className="Navbar">
-      <h1>NAV BAR</h1>
-      <nav class="Navbar">
-        <ul>
-          <li><Link exact to="/">Home</Link></li>
-          <li><Link exact to="/search">Search</Link></li>
-          <li><Link exact to="/login">Login/Register</Link></li>
-        </ul>
+      <img src="../../../assets/logo.jpg" alt="Cook book logo"/>
+      {/* <h1>ReciPlease</h1> */}
+      <nav>
+        <Link exact to="/" style={linkStyles_title}>ReciPlease</Link>
+        <Link exact to="/search" style={linkStyles_search}><input type='text' placeholder='search' id='search-bar'/></Link>
+        <Link exact to="/login" style={linkStyles_button}>Login/Register</Link>
       </nav>
     </div>
 
   );
 }
+
+const linkStyles_title = {
+  color: 'black',
+  textDecoration: 'none',
+  fontSize: '20px',
+  float: 'left',
+  padding: '10px',
+  margin: '2px, 10px, 2px, 10px',
+  textAlign: 'center',
+};
+
+const linkStyles_search = {
+  color: 'black',
+  textDecoration: 'none',
+  fontSize: '20px',
+  float: 'none',
+  padding: '10px',
+  margin: '2px, 10px, 2px, 10px',
+};
+
+const linkStyles_button = {
+  color: 'black',
+  textDecoration: 'none',
+  fontSize: '20px',
+  float: 'right',
+  padding: '10px',
+  margin: '2px, 10px, 2px, 10px',
+};
