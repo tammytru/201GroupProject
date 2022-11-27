a3c05f2a95407c7ea57b2e65cc5acb59fac01972
Tammy Truong <thtruong@usc.edu>
2022-11-27 02:52:23 -0600

    connected register form to database

--- frontend/src/pages/Login-Page/login-page.js
+++ frontend/src/pages/Login-Page/login-page.js
@@ -4,134 +4,9 @@ import ExplorePage from '../Explore-Page/explore-page';
 import { Link, Redirect } from "react-router-dom";
 import axios from '../../api/axios';
 
-/*
+
 //adapted from https://www.youtube.com/watch?v=X3qyxo_UTR4&ab_channel=DaveGray
-export default function LoginPage( {isUser, setisUser, userID, setuserID} ) {
-    const userRef = useRef();
 
-    // const [userID, setUserID] = useState();
-    const [user, setUser] = useState('');
-    const [password, setPassword] = useState('');
-    const [errorMessages, setErrorMessages] = useState({});
-
-    const errors = {
-        username: "invalid username",
-        password: "invalid password",
-        e400: "there is no account with that username and password", 
-    };
-
-    useEffect(() => {
-        userRef.current.focus();
-    }, [isUser])
-    // useEffect(() => {
-    //     setErrMsg('');
-    // }, [user, password])
-
-    const renderErrorMessage = (name) =>
-        name === errorMessages.name && (
-            <div className="error">{errorMessages.message}</div>
-    );
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-
-        const data = {
-            userName: user,
-            password: password
-        }
-        console.log(data)
-        console.log("isuser", isUser)
-
-        try {
-            const URL = "/Assignment4Backend/VerifyLogin?userName=" + user + "&password=" + password;
-            const response = await axios.post(URL,
-                //JSON.stringify({userName: user, password}),
-                data,
-                {
-                    headers: { 
-                        'Content-Type': 'application/json',
-                        'Access-Control-Allow-Origin': "*",
-                    },
-                    // withCredentials: true
-                })
-                .then((response) => {
-                    console.log(response)
-                    setuserID(response.data.userID)
-                    console.log(response.data.userID)
-                    
-                });
-            
-                if(userID != -1) {
-                    setUser('')
-                    setPassword('')
-                    setisUser(true);
-                    console.log('update isUser', isUser)
-                }
-                
-        } catch (err) {
-            console.log(err.response?.status)
-            if(!err?.response) {
-                // setErrMsg('No Server Response')
-            } else if (err.response?.status === 400) {
-                // setErrMsg('Missing username or password')
-                setErrorMessages({ name: "e400", message: errors.e400 });
-            } else if (err.response?.status === 401) {
-                // setErrMsg('Unauthorized');
-            } else {
-                // setErrMsg('Login Failed')
-            }
-            // errRef.current.focus();
-        } 
-
-    }
-
-    const renderForm = (
-        <div className="form">
-            <div className="header">
-                <h1>  User Login</h1>
-            </div>
-            <form onSubmit={handleSubmit}>
-                <div className="input-container">
-                    <label htmlFor="user">Username </label>
-                    <input 
-                        type="text" 
-                        name="userName" 
-                        ref={userRef} 
-                        onChange={(e) => setUser(e.target.value)} 
-                        value={user}
-                        required 
-                    />
-                </div>
-                <div className="input-container">
-                    <label htmlFor="password">Password </label>
-                    <input 
-                        type="password" 
-                        name="password" 
-                        onChange={(e) => setPassword(e.target.value)} 
-                        value={password}
-                        required 
-                    />
-                </div>
-                {renderErrorMessage('400')}
-                <div className="button-container">
-                    <input type="submit" />
-                    <div className="space"></div>
-                    <Link exact="true" to="/register">
-                        <button type = "register">Register</button>
-                    </Link>
-                </div> 
-            </form>
-        </div>
-    )
-
-    return (
-        <>
-            {isUser ? ( <div><ExplorePage/></div> ) : renderForm}
-        </>
-    )
-
-}
-*/
 
 export default function LoginPage( {isUser, setisUser, userID, setuserID} ) {
     const [errorMessages, setErrorMessages] = useState({});
@@ -165,9 +40,7 @@ export default function LoginPage( {isUser, setisUser, userID, setuserID} ) {
 
         try {
             const URL = "/Assignment4Backend/VerifyLogin?userName=" + user + "&password=" + password;
-            const response = await axios.post(URL,
-                //JSON.stringify({userName: user, password}),
-                data,
+            const response = await axios.post(URL, data,
                 {
                     headers: { 
                         'Content-Type': 'application/json',
@@ -199,22 +72,6 @@ export default function LoginPage( {isUser, setisUser, userID, setuserID} ) {
             }
             // errRef.current.focus();
         } 
-
-        // event.preventDefault();
-
-        // var { username, password } = document.forms[0];
-
-        // const userData = database.find((user) => user.username === username.value);
-
-        // if (userData) {
-        //     if (userData.password !== password.value) {
-        //         setErrorMessages({ name: "password", message: errors.password });
-        //     } else {
-        //         setisUser(true);
-        //     }
-        // } else {
-        //     setErrorMessages({ name: "username", message: errors.username });
-        // }
     };
 
     const renderErrorMessage = (name) =>
@@ -227,25 +84,7 @@ export default function LoginPage( {isUser, setisUser, userID, setuserID} ) {
             <div className="header">
                 <h1>  User Login</h1>
             </div>
-            {/* <form onSubmit={handleSubmit}>
-                <div className="input-container">
-                    <label>Username </label>
-                    <input type="text" name="username" required />
-                    {renderErrorMessage("username")}
-                </div>
-                <div className="input-container">
-                    <label>Password </label>
-                    <input type="password" name="password" required />
-                    {renderErrorMessage("password")}
-                </div>
-                <div className="button-container">
-                    <input type="submit" />
-                    <div className="space"></div>
-                    <Link exact to="/register">
-                        <button type = "register">Register</button>
-                    </Link>
-                </div> 
-            </form> */}
+            
             <form onSubmit={handleSubmit}>
                 <div className="input-container">
                     <label htmlFor="user">Username </label>
--- frontend/src/pages/Register-Page/register-page.js
+++ frontend/src/pages/Register-Page/register-page.js
@@ -1,69 +1,65 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Link, Redirect } from 'react-router-dom';
+import axios from '../../api/axios';
 import './styles.css';
+import { ExplorePage } from '../../pages'
 
-export default function RegisterPage( {isUser, setisUser} ) {
+export default function RegisterPage( {isUser, setisUser, userID, setuserID} ) {
 
 	// States for registration
-	const [firstName, setFirstName] = useState('');
-	const [lastName, setLastName] = useState('');
-	const [email, setEmail] = useState('');
+	const [user, setUser] = useState('');
 	const [password, setPassword] = useState('');
-	const [username, setUsername] = useState('');
+	const [hint, setHint] = useState('');
+
 
 	// States for checking the errors
 	const [submitted, setSubmitted] = useState(false);
 	const [error, setError] = useState(false);
 
-	// Handling the name change
-	const handleFirstName = (e) => {
-		setFirstName(e.target.value);
-		setSubmitted(false);
-	};
-
-	const handleLastName = (e) => {
-		setLastName(e.target.value);
-		setSubmitted(false);
-	};
-
-	// Handling the email change
-	const handleEmail = (e) => {
-		setEmail(e.target.value);
-		setSubmitted(false);
-	};
-
-	// Handling the password change
-	const handlePassword = (e) => {
-		setPassword(e.target.value);
-		setSubmitted(false);
-	};
-
-	const handleUsername = (e) => {
-		setUsername(e.target.value);
-		setSubmitted(false);
-	};
-
+	// Update userID when user is authenticated
+	useEffect(() => {
+        if(userID !== -1) {
+            setUser('')
+            setPassword('')
+			setHint('')
+            setisUser(true);
+			setSubmitted(true)
+			setError(false)
+            console.log('update isUser', isUser)
+        }
+    }, [userID]);
+	
 	// Handling the form submission
-	const handleSubmit = (e) => {
+	const handleSubmit = async (e) => {
 		e.preventDefault();
-		if (firstName === '' || lastName === '' || email === '' || password === '' || username === '') {
-			setError(true);
-		} else {
-			setSubmitted(true);
-			setError(false);
-			console.log('firstname: ', firstName);
-			console.log('lastname: ', lastName);
-			console.log('username: ', username);
-			console.log('email: ', email);
-			console.log('password: ', password);
-			setFirstName('');
-			setLastName('');
-			setUsername('');
-			setEmail('');
-			setPassword('');
 
-			setisUser(true);
+		const data = {
+			userName: user,
+			password: password,
+			hint: hint,
+			profileImage: '',
+			bio: ''
 		}
+
+		try {
+			const URL = "/Assignment4Backend/RegisterUser?userName=" + user + "&password=" + password + "&hint=" + hint + "&profileImage=" + '' + "&bio=" + '' + "&rating=" + '0';
+			//+ "&profileImage=/'/'&bio=/'/'"
+			const response = await axios.post(URL, data, {
+				headers: {
+					'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': "*",
+				}
+			})
+			.then((response) => {
+				console.log(response)
+				setuserID(response.data.userID)
+			});
+
+
+		} catch (err) {
+			console.log(err.response?.status)
+		}
+
 	};
 
 	// Showing success message
@@ -74,7 +70,7 @@ export default function RegisterPage( {isUser, setisUser} ) {
 				style={{
 					display: submitted ? '' : 'none',
 				}}>
-				<h1>User {username} successfully registered!!</h1>
+				<h1>User {user} successfully registered!!</h1>
 			</div>
 		);
 	};
@@ -92,7 +88,7 @@ export default function RegisterPage( {isUser, setisUser} ) {
 		);
 	};
 
-	return (
+	const renderForm = (
 		<div className="form">
 			<div class="header">
 				<h1> Register</h1>
@@ -106,39 +102,53 @@ export default function RegisterPage( {isUser, setisUser} ) {
 			<form onSubmit={handleSubmit}>
 				{/* Labels and inputs for form data */}
 				<div className="input-container">
-                    <label>First Name </label>
-                    <input type="text" name="fname" onChange={handleFirstName} value={firstName} required />
-                    {errorMessage()}
+                    <label htmlFor="user">Username </label>
+                    <input 
+                        type="text" 
+                        name="userName" 
+                        onChange={(e) => setUser(e.target.value)} 
+                        value={user}
+                        required 
+                    />
+					{errorMessage()}
                 </div>
 				<div className="input-container">
-                    <label>Last Name </label>
-                    <input type="text" name="lname" onChange={handleLastName} value={lastName} required />
-                    {errorMessage()}
+                    <label htmlFor="user">Password </label>
+                    <input 
+                        type="text" 
+                        name="password" 
+                        onChange={(e) => setPassword(e.target.value)} 
+                        value={password}
+                        required 
+                    />
+					{errorMessage()}
                 </div>
-
 				<div className="input-container">
-                    <label>Username </label>
-                    <input type="text" name="uname" onChange={handleUsername} value={username} required />
-                    {errorMessage()}
+                    <label htmlFor="user">Password Hint </label>
+                    <input 
+                        type="text" 
+                        name="hint" 
+                        onChange={(e) => setHint(e.target.value)} 
+                        value={hint}
+                        required 
+                    />
+					{errorMessage()}
                 </div>
-
-				<div className="input-container">
-                    <label>Email </label>
-                    <input type="email" name="email" onChange={handleEmail} value={email} required />
-                    {errorMessage()}
-                </div>
-
-				<div className="input-container">
-                    <label>Password </label>
-                    <input type="password" name="password" onChange={handlePassword} value={password} required />
-                    {errorMessage()}
-                </div>
-
 				<div className="button-container">
 					<input type="submit" />
+					<div className="space"></div>
+                    <Link exact="true" to="/login">
+                        <button type = "register">Login</button>
+                    </Link>
 				</div> 
 				{successMessage()}
 			</form>
 		</div>
+	);
+
+	return (
+		<div>
+        {isUser ? <div><ExplorePage/></div> : renderForm}
+        </div>
 	);
 }
