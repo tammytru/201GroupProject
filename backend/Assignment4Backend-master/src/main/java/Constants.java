168dad81cc4511d9f638f165695edeee1a89e5b5
Tammy Truong <thtruong@usc.edu>
2022-11-29 00:11:58 -0600

    removed backend and added .gitignore

--- backend/Assignment4Backend-master/src/main/java/Constants.java
+++
@@ -1,5 +0,0 @@
-
-public class Constants {
-	//put your sql url here, for example jdbc:mysql://localhost/project?user=root&password=yourpassword
-	public String sqlUrl = "jdbc:mysql://localhost/project?user=root&password=password";
-}
